54a754339a594a7324e9297a270f4a7140ff4b31
dlangner5023 <dlangner@uw.edu>
2016-01-24 23:23:53 -0800

    added Network Error bar, UI search bar option, editted UI

--- Flicks/MovieViewController.swift
+++ Flicks/MovieViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AFNetworking
 import SwiftLoader
 
-class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     var refreshControl: UIRefreshControl!
     
@@ -18,53 +18,93 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
  
     @IBOutlet weak var scrollView: UIScrollView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var networkErrorButton: UIButton!
+    
     var config : SwiftLoader.Config = SwiftLoader.Config()
     
     var movies: [NSDictionary]?
-
+    
+    var filteredMovies: [NSDictionary]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        config.size = 150
-        config.spinnerColor = .blueColor()
-        config.foregroundColor = .redColor()
-        config.foregroundAlpha = 0.5
-        SwiftLoader.setConfig(config)
-
+        
+        self.tableView.backgroundColor = UIColor(white: 0.15, alpha: 1.0)
+        
+        searchBar.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
         
+        self.loadingScreen()
+        
+        self.fetchMovieData()
+        self.controlRefresh()
+
+    }
+    
+    func controlRefresh() {
+        
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        self.scrollView.insertSubview(refreshControl, atIndex: 0)
+    
+    }
+    
+    func loadingScreen() {
+        
+        config.size = 150
+        config.spinnerColor = .redColor()
+        config.foregroundColor = .blackColor()
+        config.foregroundAlpha = 1.0
+        SwiftLoader.setConfig(config)
+    
+    }
+    
+    @IBAction func networkErrorButtonPressed(sender: AnyObject) {
+        self.onRefresh()
+    }
+    
+    func fetchMovieData() {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
             delegate:nil,
-            delegateQueue:NSOperationQueue.mainQueue()
-        )
+            delegateQueue:NSOperationQueue.mainQueue())
+        
+        self.networkErrorButton.backgroundColor = UIColor.darkGrayColor()
+        self.networkErrorButton.titleLabel!.textColor = UIColor.whiteColor()
         
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
+                
+                self.refreshControl.endRefreshing()
+                
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
                             
                             self.movies = responseDictionary["results"] as? [NSDictionary]
+                            self.filteredMovies = self.movies
+                            self.networkErrorButton.hidden = true
                             self.tableView.reloadData()
-                            
+                    }
+                } else {
+                    if error != nil {
+                        self.networkErrorButton.hidden = false
                     }
                 }
         });
         task.resume()
-        
-        refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
-        scrollView.insertSubview(refreshControl, atIndex: 0)
-
+    
+    
+    
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
@@ -88,15 +128,13 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     func onRefresh() {
-        delay(2, closure: {
-            self.refreshControl.endRefreshing()
-        })
+        self.fetchMovieData()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if let movies = movies {
-            return movies.count
+        if let unwrappedFilteredMovies = filteredMovies {
+            return unwrappedFilteredMovies.count
         } else {
             return 0
         }
@@ -106,7 +144,7 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
-        let movie = movies![indexPath.row]
+        let movie = self.filteredMovies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         
@@ -117,19 +155,20 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         cell.titelLabel.text = title
         cell.overviewLabel.text = overview
         cell.posterView.setImageWithURL(imageUrl!)
+        
+        cell.titelLabel.textColor = UIColor.orangeColor()
+        cell.overviewLabel.textColor = UIColor.whiteColor()
+        cell.backgroundColor = UIColor.clearColor()
 
         return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredMovies = searchText.isEmpty ? movies : movies!.filter({(movie: NSDictionary) -> Bool in
+            return (movie["title"] as! String).rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+        self.tableView.reloadData()
+        
     }
-    */
-
+ 
 }
